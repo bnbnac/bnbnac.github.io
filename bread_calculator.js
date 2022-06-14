d226995e4bc575ffe24759794cf52274babe4913
SJ <100001092+bnbnac@users.noreply.github.com>
2022-06-14 18:30:24 +0900

    Add files via upload
    version 1
    need update
    no reason for two buttons ( selThick, selPi)....
    make one button which has onclick (initiates calThick, calPi)

---
+++ bread_calculator.js
@@ -0,0 +1,70 @@
+
+const selPi = document.getElementById("selPi");
+const selThick = document.getElementById("selThick");
+const calPi = document.getElementById("calPi");
+const calThick = document.getElementById("calThick");
+
+const samPi = document.getElementById("samPi");
+const samThick = document.getElementById("samThick");
+const samWeight = document.getElementById("samWeight");
+const proWeight = document.getElementById("proWeight");
+const proPi = document.getElementById("proPi");
+const proThick = document.getElementById("proThick");
+
+// // const density = samWeight / (((samPi / 2) ** 2) * samThick);
+// const calThick = (proWeight.value / ((proPi.value / 2) ** 2)) / (samWeight.value / (((samPi.value / 2) ** 2) * samThick.value));
+// const calPi = (proWeight.value / proThick.value) / (samWeight.value / (((samPi.value / 2) ** 2) * samThick.value));
+
+function inPi(event){
+    if(samPi.value * samThick.value * samWeight.value * proWeight.value > 0) {
+        event.preventDefault();
+        samPi.disabled=true;
+        samThick.disabled=true;
+        samWeight.disabled=true;
+        proWeight.disabled=true;
+        inputProPi();
+    } else {
+        event.preventDefault(); 
+        alert("<샘플>의 [파이,두께,무게], <공정>의 [무게]를 입력하세요");
+    }
+}
+function inThick(event){
+    if(samPi.value * samThick.value * samWeight.value * proWeight.value > 0) {
+        event.preventDefault();
+        samPi.disabled=true;
+        samThick.disabled=true;
+        samWeight.disabled=true;
+        proWeight.disabled=true;
+        inputProThick();
+    } else {
+        event.preventDefault(); 
+        alert("<샘플>의 [파이,두께,무게], <공정>의 [무게]를 입력하세요");
+    }
+}
+function inputProPi(){
+    proThick.disabled=false;
+    proThick.placeholder="두께 입력"
+    calThick.style="position:absolute;top: 330px;left: 1080px;width: 100px;height: 20px;"
+
+}
+function inputProThick(){
+    proPi.disabled=false;
+    proPi.placeholder="파이 입력"
+    calPi.style="position:absolute;top: 250px;left: 720px;width: 100px;height: 20px;"
+
+}
+
+function lastPi(event) {
+    event.preventDefault();
+    proThick.value = (proWeight.value / ((proPi.value / 2) ** 2)) / (samWeight.value / (((samPi.value / 2) ** 2) * samThick.value));
+}
+
+function lastThick(event) {
+    event.preventDefault();
+    proPi.value = Math.sqrt(4 * ((proWeight.value / proThick.value) / (samWeight.value / (((samPi.value / 2) ** 2) * samThick.value))));
+}
+
+selPi.addEventListener("click", inPi);
+selThick.addEventListener("click", inThick);
+calPi.addEventListener("click", lastPi);
+calThick.addEventListener("click", lastThick);
